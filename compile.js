de9bf1258f5aba8a517640133b965ed320c4e3a7
timaevV <timaevvladislav@gmail.com>
2024-01-17 02:51:23 +0600

    feat: update contract deploy and compile logic

--- compile.js
+++ compile.js
@@ -1,8 +1,28 @@
-const path = require("path")
-const fs = require("fs")
-const solc = require("solc")
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol")
-const source = fs.readFileSync(lotteryPath, "utf8")
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
+const source = fs.readFileSync(lotteryPath, 'utf8')
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"]
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Lottery.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+};
+
+const compiledOutput = solc.compile(JSON.stringify(input));
+const contractName = 'Lottery';
+const contract = JSON.parse(compiledOutput).contracts['Lottery.sol'][contractName];
+
+module.exports = contract;
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,30 +1,37 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address public lastWinner;
+    address payable[] public players;
 
-    function Lottery() public {
-       manager = msg.sender;
+    constructor() {
+        manager = msg.sender;
     }
 
     function enter() public payable {
       require(msg.value > .01 ether);
-      players.push(msg.sender);
+      players.push(payable(msg.sender));
     }
 
     function random() private view returns (uint) {
-       return uint(keccak256(block.difficulty, now, players));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
 
     function pickPlayerWinner() public restricted {
+        require(players.length > 0, "No players to pick a winner from");
+
         uint index = players.length % random();
-        players[index].transfer(this.balance);
+        require(index < players.length, "Invalid index");
+
+        players[index].transfer(address(this).balance);
         lastWinner = players[index];
-        players = new address[](0);
+        delete players;
     }
 
-    function getPlayers() public view returns (address[]) {
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
 
